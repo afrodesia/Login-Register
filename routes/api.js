f70b45e2038f133e54acae1cd7d4f91d73d86af4
Ken Torry <kentorry@afrodesiamedia.com>
2017-03-05 11:48:36 -0600

    Update profile resource for post and created form on index for testing

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,35 @@ var express = require('express')
 var router = express.Router()
 var Profile = require('../models/Profile')
 
+router.post('/:resource', function(req, res, next){
+	var resource = req.params.resource
+
+	if(resource === 'profile'){
+
+		var formData = req.body
+		Profile.create(formData, function(err, profile){
+			if(err){
+				res.json({
+					confirmation: 'fail',
+					message: err
+				})
+				return
+			}
+			res.json({
+				confirmation: 'success',
+				result: profile
+			})
+		})
+		return
+	}
+	res.json({
+		confirmation: 'fail',
+		message: 'Resource '+resource+' not supported..'
+	})
+})
 router.get('/:resource', function(req, res, next){
 	var resource = req.params.resource
-	if(resource == 'profile'){
+	if(resource === 'profile'){
 		Profile.find(null, function(err, profiles){
 
 			if(err){
