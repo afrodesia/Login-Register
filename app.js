119cd49f3098d7d664e85993bb8c15499d5c296e
Ken Torry <kentorry@afrodesiamedia.com>
2017-03-12 21:55:41 -0500

    added sessions in app

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var sessions = require('client-sessions');
 
 var dbUrl = 'mongodb://localhost/profile-signup'
 mongoose.connect(dbUrl, function(err, res){
@@ -33,6 +34,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(sessions({
+  cookieName:'session',
+  duration:24*60*60*1000,
+  secret: 'mikagokuzorion6',
+  activeDuration: 24*60*1000
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
