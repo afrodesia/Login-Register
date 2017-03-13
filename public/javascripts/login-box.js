58ca02072666c0f6b16765c1d44a0a78a0726a3c
Ken Torry <kentorry@afrodesiamedia.com>
2017-03-13 00:45:09 -0500

    cleaned up html - re-styled form element

---
+++ public/javascripts/login-box.js
@@ -0,0 +1,28 @@
+// (function(){
+
+// 	//   $('#loginButton').on('click',function(){
+
+
+// 	//     $(this).next('#loginBox').slideToggle();
+// 	//     $(this).toggleClass('active'); 
+// 	//     e.stopPropagation();         
+// 	// });
+
+// 	  $('#loginButton').click(function () {
+//         if ($('#loginBox').is(":visible")) {
+//             $('#loginBox').hide()
+// 			$('#loginButton').removeClass('active');
+//         } else {
+//             $('#loginBox').show()
+// 			$('#loginButton').addClass('active');
+//         }
+// 		return false;
+//     });
+// 	$('#loginBox').click(function(e) {
+//         e.stopPropagation();
+//     });
+//     $(document).click(function() {
+//         $('#signin-dropdown').hide();
+// 		$('#session').removeClass('active');
+//     });
+// })()
